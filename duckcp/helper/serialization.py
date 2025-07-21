bb2bc06ec38ae7b36454867603c22a5aa421e14d
redraiment <redraiment@gmail.com>
2025-07-21 13:24:44 +0800

    升级至v0.1.1
    * 支持decimal类型数据。

--- duckcp/helper/serialization.py
+++ duckcp/helper/serialization.py
@@ -1,8 +1,20 @@
 """
 序列化与反序列化帮助函数。
 """
-import json
+from decimal import Decimal
+from json import JSONEncoder, dumps, loads
 from typing import Any
+
+
+class JsonEncoder(JSONEncoder):
+    """
+    序列化JSON的数据：
+    - 添加Decimal类型支持。
+    - 其他类型数据通过`vars`支持。
+    """
+
+    def default(self, value) -> any:
+        return float(value) if isinstance(value, Decimal) else vars(value)
 
 
 def json_encode(data: Any) -> str:
@@ -12,11 +24,11 @@ def json_encode(data: Any) -> str:
     - 去掉多于空格
     - 支持自定义的类型
     """
-    return json.dumps(data, separators=(',', ':'), ensure_ascii=False, default=vars)
+    return dumps(data, separators=(',', ':'), ensure_ascii=False, cls=JsonEncoder)
 
 
 def json_decode(data: str | bytes) -> any:
     """
     将JSON字符串反序列化成对象
     """
-    return json.loads(data)
+    return loads(data)
